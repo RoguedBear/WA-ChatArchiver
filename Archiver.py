739a1a01b204d17c28d7ad8734968585f2adc19b
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2020-04-28 17:27:40 +0530

    Added Telegram integration; commenting
    commented what class names belong to which location of the WA Web UI

--- Archiver.py
+++ Archiver.py
@@ -6,7 +6,17 @@ from selenium.webdriver.chrome.options import Options
 from time import sleep
 from datetime import datetime
 from sys import stdout
+import requests
+
 # Start script by setting chromedriver in the environmental PATH variable
+def telegram_bot_sendtext(*bot_message):
+    bot_message = ' '.join(bot_message)
+    bot_token = 'YOUR TOKEN'
+    bot_chatID = 'YOUR CHAT ID'
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+
+    response = requests.get(send_text)
+
 
 #Hello message:
 print("\t\x1b[1;31m Script created by \x1b[1m\x1b[5;32;40mROGUEDBEAR\x1b[0m")
@@ -21,7 +31,7 @@ def archive():
         hover.perform()
 
 
-        z = driver.find_element_by_class_name("ZR5SB")
+        z = driver.find_element_by_class_name("ZR5SB") # ZR5SB is the class name of down arrow
         z.click()
 
     except:
@@ -30,12 +40,14 @@ def archive():
     # sleep(5)
     while True:
         try:
-            archive_button = driver.find_element_by_class_name("_167q")
+            archive_button = driver.find_element_by_class_name("_167q") # Middle part of class name of the archive button
             archive_button.click()
             print("FOUND: Archive button.")
             return 1
         except:
-            print("NOT FOUND")
+            print("NOT FOUND, retrying")
+
+
             return 0
 
 
@@ -65,8 +77,11 @@ else:
     driver.get("https://web.whatsapp.com")
 
 # Wait for the page to load
+print("Waiting for page to load...")
 search_bar = WebDriverWait(driver, 60).until(lambda r: driver.find_element_by_class_name("selectable-text"))
+print("Page loaded!")
 
+telegram_bot_sendtext("Program Started")
 # select search if not; retrieve the current number and clear and all
 print(f"[{datetime.now().strftime('%H:%M')}]", "Search Bar Found")
 if search_bar.text in id:
@@ -80,13 +95,14 @@ else:
 # Ensuring only one chat is there:
 # if more chats are there, redo the search thingy
 while True:
-    if len(driver.find_elements_by_class_name('_2wP_Y')) == 2:
+    if len(driver.find_elements_by_class_name('_2wP_Y')) == 2: # _2wP_Y is the class name of each row/column chat present on the left side
         print(f"[{datetime.now().strftime('%H:%M')}]", "Only one chat found. Very good😏...")
         break
     else:
         print(f"[{datetime.now().strftime('%H:%M')}]", "More than one chat present. re-searching...")
         search_bar.clear()
         search_bar.send_keys(id)
+        WebDriverWait(driver, 10).until(lambda r: len(driver.find_elements_by_class_name("_2wP_Y")) == 2)
 
 # TODO: archive test(); imp:
 try:
@@ -94,7 +110,7 @@ try:
         if len(driver.find_elements_by_class_name('_2wP_Y')) == 2:
             try: # If archive element is present or not
                 # driver.implicitly_wait(5)
-                if_archived = driver.find_element_by_class_name("_3JEcM")
+                if_archived = driver.find_element_by_class_name("_3JEcM") # _3JEcM is the class name of Archived box
                 if if_archived.text == 'Archived':
                     print(f"[{datetime.now().strftime('%H:%M')}]", "archived...", end='\r')
                     continue
@@ -108,7 +124,16 @@ try:
                         try:
                             WebDriverWait(driver, 10).until(lambda r: driver.find_element_by_class_name("_3JEcM"))
                             print(f"[{datetime.now().strftime('%H:%M')}]", "Archive CONFIRMED")
+                            telegram_bot_sendtext(f"[{datetime.now().strftime('%H:%M')}]", "Archived now")
+
+                            # try to click the textbox of typing text if visible
+                            try:
+                                chat_textbox = driver.find_element_by_class_name("_2WovP")
+                                chat_textbox.click()
+                            except:
+                                pass
                         except:
+                            print(f"[{datetime.now().strftime('%H:%M')}]", "Couldn't archive chat. Trying again.")
                             continue
                     elif result ==0:
                         continue
@@ -116,9 +141,13 @@ try:
                     continue
         else:
             print(f"[{datetime.now().strftime('%H:%M')}]", "More than one chat, probably on homescreen. Researching...")
+
             search_bar.clear()
             search_bar.send_keys(id)
-            sleep(0.5)
+            try:
+                WebDriverWait(driver, 5).until(lambda r: len(driver.find_elements_by_class_name("_2wP_Y")) == 2)
+            except :
+                continue
 except KeyboardInterrupt:
     print("Bye Bye!")
     driver.quit()
